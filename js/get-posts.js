1d13651925098b88ce8cefdbbdaa7b9a701f4f77
aaronhans <aaronhans@gmail.com>
2019-07-10 13:50:31 -0700

    everything there and in close to correct position

--- js/get-posts.js
+++ js/get-posts.js
@@ -1,34 +1,41 @@
-fetch('js/temp-posts.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    template(myJson)
-  });
+let altDescription = "God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs. Checkmate... Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should.";
+
+fetch('https://coprocure.github.io/coprocure.us/feed.json')
+.then(function(response) {
+  return response.json();
+})
+.then(function(myJson) {
+  template(myJson)
+});
 
 function template(json) {
   output = `<h4>Insights, trends, and our latest news</h4>
     <macro-carousel pagination>
     ${json.items.map((item, index) => {
       let description = item.description;
-      if(description.indexOf('<figure>') > -1) {
-        description = description.split('figure>')[1];
+      if(!description) {
+        description = altDescription;
       }
-       return `<article class="slide">
+      let output = '';
+      if(index % 4 === 0) {
+        output += `<article class="slide">`;
+      }
+      output += `
         <div class="carousel-card card--blog">
-          <img src="${item.thumbnail}" alt="" style="max-width: 100px;" />
-          <h5>${item.title}</h5>
-          <p class="blog-summary">blog description</p>
-        </div>
-      </article>`;
+          <div class="picture-cropper">
+            <a href="${item.link}"><img src="${item.thumbnail}" alt="" /></a>
+          </div>
+          <a href="${item.link}" class="card-text">
+            <h5>${item.title}</h5>
+            <p class="blog-summary">${description.substr(0,170)}</p>
+          </a>
+        </div>`;
+      if(index % 4 === 3) {
+        output += `</article>`;
+      }
+      return output;
     }).join('\n      ')}
     </macro-carousel>`;
 
   document.querySelector('.blog-carousel').innerHTML = output;
 }
-
-// style the cards
-// put 4 cards on each slide
-
-// this looks like place to retrieve data dynamically: https://medium.com/feed/coprocure
-// write lambda that saves that to json every couple hours
