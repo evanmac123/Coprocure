81973c72a273aafcf0a422409cf331659c117ec4
aaronhans <aaronhans@gmail.com>
2019-07-22 11:15:14 -0700

    in progress search results and contract detail page

--- js/animations.js
+++ js/animations.js
@@ -1,29 +1,25 @@
 
 var animationDelay = 2500;
 
-animateHeadline(document.querySelector('.cd-headline'));
-
 function animateHeadline(headline) {
   setTimeout(function(){ hideWord( headline.querySelector('.is-visible') ) }, animationDelay);
-  //other checks here ...
 }
 
 function hideWord(word) {
   var nextWord = takeNext(word);
   if(nextWord) {
     switchWord(word, nextWord);
-    setTimeout(function(){ 
-      hideWord(nextWord) 
+    setTimeout(function() {
+      hideWord(nextWord)
     }, animationDelay);
   }
 }
 
 function takeNext(word) {
   let nextNode = word.nextElementSibling;
+  console.log(nextNode)
   if(!nextNode && document.querySelector('.cd-headline')) {
     nextNode = document.querySelector('.cd-headline').querySelector('b');
-  } else {
-    return null;
   }
   return nextNode;
 }
@@ -37,13 +33,4 @@ function switchWord(oldWord, newWord) {
   }, 600)
 }
 
-
-document.querySelector('.burger-menu').addEventListener('click', function(event) {
-  if(this.classList.contains('is-active')) {
-    this.classList.remove('is-active');
-    document.querySelector('navigation .links').classList.remove('is-active');
-  } else {
-    this.classList.add('is-active');
-    document.querySelector('navigation .links').classList.add('is-active');
-  }
-})
+animateHeadline(document.querySelector('.cd-headline'));
