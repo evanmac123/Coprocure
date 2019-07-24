41ef696985a931e770936d6060d973f043abf90d
aaronhans <aaronhans@gmail.com>
2019-07-24 12:52:17 -0700

    now with sorting

--- js/animations.js
+++ js/animations.js
@@ -1,19 +1,15 @@
 
-let animationDelay = 2500;
-
 function animateHeadline() {
   let headline = document.querySelector('.cd-headline');
-  hideWord( headline.querySelector('.is-visible') )
-  // setTimeout(function(){ hideWord( headline.querySelector('.is-visible') ) }, animationDelay);
+  if(headline) {
+    hideWord( headline.querySelector('.is-visible') )
+  }
 }
 
 function hideWord(word) {
   var nextWord = takeNext(word);
   if(nextWord) {
     switchWord(word, nextWord);
-    /* setTimeout(function() {
-      hideWord(nextWord)
-    }, animationDelay); */
   }
 }
 
@@ -34,19 +30,11 @@ function switchWord(oldWord, newWord) {
   }, 650)
 }
 
-
-// animateHeadline(document.querySelector('.cd-headline'));
 let headlineSwitcher = window.setInterval(animateHeadline, 2500)
 
 window.addEventListener('focus', function() {
   headlineSwitcher = window.setInterval(animateHeadline, 2500)
 });
-
-
-// setup animation to run on a window setinterval
-// set that up outside function so runs onload
-// clearinterval onblur
-// reset interval onfocus
 
 window.addEventListener('blur', function() {
   window.clearInterval(headlineSwitcher)
