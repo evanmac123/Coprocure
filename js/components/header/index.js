c72733ea337316e1688ce5f0312560714e0a3904
aaronhans <aaronhans@gmail.com>
2019-07-24 09:25:59 -0700

    add spinner and url routing

--- js/components/header/index.js
+++ js/components/header/index.js
@@ -1,11 +1,12 @@
 import { render } from './template.js';
-import '../../search-run.js';
 import { burger } from '../../burger.js';
+import { search } from './search-run.js';
 
 export default class CoProcureHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = render();
     burger();
+    search();
   }
 }
 
