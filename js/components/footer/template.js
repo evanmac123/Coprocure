62fdb55ef1d07a510e1abc1ea3800422063ca9f9
Evan Mceldowney <evanmceldowney@gmail.com>
2019-07-29 11:00:55 -0700

    styling clean ups and career

--- js/components/footer/template.js
+++ js/components/footer/template.js
@@ -11,51 +11,55 @@ export function render() {
             <input name="fullname" type="text" />
             <label>Email</label>
             <input name="email" type="text" />
+      </div>
+            <div class="message-info">
+              <label>Message</label>
+              <textarea name="description"></textarea>
+            </div>
           </div>
-          <div class="message-info">
-            <label>Message</label>
-            <textarea name="description"></textarea>
+          <div class="">
+            <button class="highlight">Submit</button>
           </div>
-        </div>
-        <div class="">
-          <button class="highlight">Submit</button>
-        </div>
       </form>
     </div>
   </section>
   <footer>
-  <div class="section-interior">
-    <div class="tagline">
-      <h2 class="some-big dark-emphasis">We're committed to making government more transparent, efficient, and inclusive</h2>
-      <div class="social-links">
-        <a href="https://medium.com/coprocure" target="_new">
-          <img src="img/social/Medium.svg" alt="Medium logo" />
-        </a>
-        <a href="https://www.linkedin.com/company/coprocure/" target="_new">
-          <img src="img/social/LinkedIN.svg" alt="LinkedIn logo" />
-        </a>
-        <a href="mailto:hello@coprocure.us" target="_new">
-          <img src="img/social/Mail.svg" alt="Mail icon" />
-        </a>
-        <a href="https://twitter.com/coprocure" target="_new">
-          <img src="img/social/Twitter.svg" alt="Twitter logo" />
-        </a>
+    <div class="section-interior">
+      <div class="row info">
+        <div class="tagline">
+          <h3 class="some-big dark-emphasis">We're committed to making government more transparent, efficient, and inclusive</h3>
+          <div class="social-links">
+            <a href="https://medium.com/coprocure" target="_new">
+      <img src="img/social/Medium.svg" alt="Medium logo" />
+    </a>
+            <a href="https://www.linkedin.com/company/coprocure/" target="_new">
+      <img src="img/social/LinkedIN.svg" alt="LinkedIn logo" />
+    </a>
+            <a href="mailto:hello@coprocure.us" target="_new">
+      <img src="img/social/Mail.svg" alt="Mail icon" />
+    </a>
+            <a href="https://twitter.com/coprocure" target="_new">
+      <img src="img/social/Twitter.svg" alt="Twitter logo" />
+    </a>
+          </div>
+        </div>
+        <div class="us-links">
+          <a href="#">About</a>
+          <a href="https://medium.com/coprocure" target="_new">Blog</a>
+          <a href="https://medium.com/coprocure/coprocure-is-hiring-a-backend-engineer-2e8780b30c55" target="_blank">Careers</a>
+          <a href="#">For Suppliers</a>
+        </div>
+      </div>
+      <div class="copyright">
+        <div class="section-interior down-low">
+          <span class="copyright">&#0169; 2019 by CoProcure. All rights reserved.</span>
+          <div class="bottom-links">
+            <a href="https://www.coprocure.us/add.html">Share Your Contracts</href>
+  <a href="https://www.coprocure.us/files/CoProcure-privacy-policy.pdf" target="_new">Privacy Policy</a>
+            <a href="https://www.coprocure.us/files/CoProcure-terms-of-use.pdf" target="_new">Terms of Use</a>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="us-links">
-      <a href="#">About</a>
-      <a href="https://medium.com/coprocure" target="_new">Blog</a>
-      <a href="https://medium.com/coprocure/coprocure-is-hiring-a-backend-engineer-2e8780b30c55" target="_blank">Careers</a>
-      <a href="#">For Suppliers</a>
-    </div>
-  </div>
-  <div class="section-interior down-low">
-    <span class="copyright">&#0169; 2019 by CoProcure. All rights reserved.</span>
-    <div class="bottom-links">
-      <a href="https://www.coprocure.us/add.html">Share Your Contracts</href>
-      <a href="https://www.coprocure.us/files/CoProcure-privacy-policy.pdf" target="_new">Privacy Policy</a>
-      <a href="https://www.coprocure.us/files/CoProcure-terms-of-use.pdf" target="_new">Terms of Use</a>
-    </div>
-  </div>
-</footer>`;
+  </footer>`;
 }
--- js/components/header/template.js
+++ js/components/header/template.js
@@ -9,12 +9,12 @@ export function render() {
       <span class="company-name">CoProcure</span>
     </a>
 
-    <form class="search-container" style="display: none;">
+    <form class="search-container">
       <input class="search-box" autocomplete="off" name="coprocure_query" placeholder="Keyword, supplier, lead agency...">
       <svg class="search-icon" width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M18.053 11.8788C18.053 15.0669 15.4547 17.6607 12.238 17.6607C9.02125 17.6607 6.42301 15.0669 6.42301 11.8788C6.42301 8.6907 9.02125 6.09683 12.238 6.09683C15.4547 6.09683 18.053 8.6907 18.053 11.8788Z" stroke="#37A8EB" stroke-width="2.32599"></path>
         <path d="M16.3083 16.5088L23.8678 23.4537" stroke="#37A8EB" stroke-width="2.32599"></path>
-      </svg>            
+      </svg>
       <button class="search-now">Search</button>
     </form>
 
@@ -27,7 +27,7 @@ export function render() {
       <circle cx="13.75" cy="13.75" r="6.5431" stroke="#37A8EB" stroke-width="3.41379"/>
       <path d="M18.5625 19.25L27.5 27.5" stroke="#37A8EB" stroke-width="3.41379"/>
     </svg>
-      
+
     <div class="burger-menu hamburger hamburger--squeeze">
       <div class="hamburger-box">
         <div class="hamburger-inner"></div>
