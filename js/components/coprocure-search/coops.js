c8105c6b66843d057d01ef80d7fa540e9702554a
aaronhans <aaronhans@gmail.com>
2019-07-31 17:49:46 -0700

    remove coop stragglers until data is fixed

--- js/components/coprocure-search/coops.js
+++ js/components/coprocure-search/coops.js
@@ -1,22 +1,3 @@
 export function coops() {
-  return ["1Government Procurement Alliance (1GPA)", "Choice Partners", "E&I Cooperative Services", "Allied States Cooperative", "HGACBuy", "Kansas City Regional Purchasing Cooperative (KCRPC)", "NASPO ValuePoint", "National Cooperative Purchasing Alliance (NCPA)", "Sourcewell", "The Interlocal Purchasing System (TIPS)", "U.S. Communities and National IPA (OMNIA)"]
+  return ["1Government Procurement Alliance (1GPA)", "Choice Partners", "E&I Cooperative Services", "Allied States Cooperative", "HGACBuy", "Kansas City Regional Purchasing Cooperative (KCRPC)", "National Cooperative Purchasing Alliance (NCPA)", "Sourcewell", "The Interlocal Purchasing System (TIPS)"]
 }
-
-/*
-
-
------- cool ---
-has results:
-- 1Government Procurement Alliance (1GPA)
-- Choice Partners
-
-
-saved queries:
-- https://1lnhd57e8f.execute-api.us-west-1.amazonaws.com/prod?q.parser=structured&size=10&start=0&q=(and%20%20cooperative_affiliation:%27Choice%20Partners%27%20)
-
-U.S. Communities and National IPA (OMNIA)	OMNIA Partners	Is there a way to tag U.S. Communities and National IPA so these keywords are associated with OMNIA contracts?
-	Minnesota Multistate Contracting Alliance for Pharmacy (MMCAP)	Add - in database already.
-	BuyBoard	Add - in database already. Coop and lead agency should both be BuyBoard. Should be a coop drop-down selection.
-  Texas Department of Information Resources (DIR)	Add - in database already. Coop and lead agency should both be Texas Department of Information Resources (DIR).
-  
-*/
